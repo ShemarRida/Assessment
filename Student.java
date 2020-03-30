8201c95f25adff9877b6bd4f9c4039bf94e2817d
Shemar Rida <shemarrida1227@gmail.com>
2020-03-30 14:27:44 -0400

    added files

---
+++ Student.java
@@ -0,0 +1,19 @@
+package assessment;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private double scores[];
+
+    public Student(String firstName, String lastName, double scores[ ]) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.scores = scores;
+    }
+    public Double totalScores(){
+
+    }
+
+
+}
+
---
+++ StudentTest.java
@@ -0,0 +1,7 @@
+package assessment;
+
+public class StudentTest {
+    public static void main(String[] args) {
+       Student s1 = new Student("Mike", "Jones", 69.0, 78.0, 88.0);
+    }
+}
