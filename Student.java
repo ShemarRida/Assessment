22d12a41ddc01edce03a314c5621a940b77e5efe
ShemarRida <61988621+ShemarRida@users.noreply.github.com>
2020-03-30 17:59:15 -0400

    Updated changes

--- Student.java
+++ Student.java
@@ -1,19 +1,76 @@
 package assessment;
 
+import java.util.Arrays;
+
 public class Student {
     private String firstName;
     private String lastName;
-    private double scores[];
+    private Double[] scores;
 
-    public Student(String firstName, String lastName, double scores[ ]) {
+    public Student(String firstName, String lastName, Double[] scores) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.scores = scores;
     }
-    public Double totalScores(){
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double[] getScores() {
+        return scores;
+    }
+
+    public void setScores(Double[] scores) {
+        this.scores = scores;
     }
 
 
+    public double totalScores(Double[] scores) {
+        double sum = 0;
+        for (int x = 0; x < this.scores.length; x++) {
+            sum += this.scores[x];
+        }
+        return sum;
+    }
+
+    public double averageScore(Double[] scores) {
+        double sum = 0;
+        for(int x = 0; x < this.scores.length; x++) {
+            sum += this.scores[x];
+        }
+            double average = sum / this.scores.length;
+            return average;
+        }
+
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", scores=" + Arrays.toString(scores) +
+                '}';
+    }
 }
+
+
+
+
+
+
+
 
--- StudentTest.java
+++ StudentTest.java
@@ -1,7 +1,22 @@
 package assessment;
 
+
+import java.util.Arrays;
+
 public class StudentTest {
     public static void main(String[] args) {
-       Student s1 = new Student("Mike", "Jones", 69.0, 78.0, 88.0);
+      Double[] scores = new Double[]{78.0, 88.0, 90.0};
+      Student s1 = new Student("Shemar", "Rida", scores);
+
+
+        System.out.println(s1);
+        System.out.println("Shemar's scores are :" + Arrays.toString(scores));
+        System.out.println("Shemar's total score is :" + s1.totalScores(scores) );
+        System.out.println("Shemar's average score is :" + s1.averageScore(scores));
+
+
+
+
+
     }
 }
